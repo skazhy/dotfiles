9b7d373d85eb91f54d09d0db5a6bd5b2968cffa3
Karlis Lauva <skazhy@gmail.com>
2015-03-17 16:14:51 -0600

    add clojars

--- Clojure/profiles.clj
+++ Clojure/profiles.clj
@@ -1,4 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-bikeshed "0.2.0"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.2.1"]]}}
+                  [jonase/eastwood "0.2.1"]]}
+ :auth {:repository-auth {#"https://clojars.org/repo"
+                         {:username "karlis"
+                          :password :env/clojars_password}}}}
