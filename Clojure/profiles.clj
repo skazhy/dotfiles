cc72e2960e38b9989ad9a6b035d0145230aa8014
Karlis Lauva <k@karlis.me>
2015-10-02 11:21:21 -0600

    lein: add codox

--- Clojure/profiles.clj
+++ Clojure/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-bikeshed "0.2.0"]
                   [lein-kibit "0.0.8"]
+                  [codox "0.6.4"]
                   [jonase/eastwood "0.2.1"]]}
  :auth {:repository-auth {#"https://clojars.org/repo"
                          {:username "karlis"
