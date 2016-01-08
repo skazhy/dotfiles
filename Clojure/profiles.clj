05ed97cdbee90c77503c25808ed572d4ce983341
Karlis Lauva <k@karlis.me>
2016-01-08 14:27:07 +0200

    lein bump plugin versions

--- Clojure/profiles.clj
+++ Clojure/profiles.clj
@@ -1,8 +1,10 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+{:user {:plugins [[cider/cider-nrepl "0.10.1"]
                   [lein-bikeshed "0.2.0"]
+                  [lein-cloverage "1.0.7-SNAPSHOT"]
+                  [lein-cljfmt "0.3.0"]
                   [lein-kibit "0.0.8"]
                   [codox "0.6.4"]
-                  [jonase/eastwood "0.2.1"]]}
+                  [jonase/eastwood "0.2.3"]]}
  :auth {:repository-auth {#"https://clojars.org/repo"
                          {:username "karlis"
                           :password :env/clojars_password}}}}
