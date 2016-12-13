027154b2395eb53c48c7c9dc5ae16e0868346366
Karlis Lauva <k@karlis.me>
2016-12-13 12:19:54 -0700

    update lein profile

--- Clojure/profiles.clj
+++ Clojure/profiles.clj
@@ -1,11 +1,12 @@
-{:user {:plugins [[cider/cider-nrepl "0.12.0"]
+{:user {:plugins [[cider/cider-nrepl "0.10.1"]
                   [lein-bikeshed "0.2.0"]
                   [lein-cloverage "1.0.7-SNAPSHOT"]
-                  [lein-cljfmt "0.3.0"]
+                  [com.jakemccrary/lein-test-refresh "0.17.0"]
                   [lein-kibit "0.0.8"]
                   [lein-try "0.4.3"]
                   [codox "0.6.4"]
-                  [jonase/eastwood "0.2.3"]]}
+                  [jonase/eastwood "0.2.3"]]
+          :aliases {"lint" ["do" "eastwood," "bikeshed"]}}
  :auth {:repository-auth {#"https://clojars.org/repo"
                          {:username "karlis"
                           :password :env/clojars_password}}}}
