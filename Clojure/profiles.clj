fe1927a11e80ffefa6a25f5e0d063b3f51392494
Karlis Lauva <k@karlis.me>
2017-09-29 14:28:36 +0300

    add lein yagni

--- Clojure/profiles.clj
+++ Clojure/profiles.clj
@@ -4,6 +4,7 @@
                   [lein-kibit "0.0.8"]
                   [lein-try "0.4.3"]
                   [codox "0.6.4"]
+                  [venantius/yagni "0.1.4"]
                   [jonase/eastwood "0.2.3"]]
           :aliases {"lint" ["do" "eastwood," "bikeshed"]}}
  :auth {:repository-auth {#"https://clojars.org/repo"
