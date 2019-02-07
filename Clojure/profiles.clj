4558d399fbe916633a1c14bd6d8979318e9c470f
Karlis Lauva <k@karlis.me>
2019-02-07 16:29:32 +0200

    lein: bump plugin versions

--- Clojure/profiles.clj
+++ Clojure/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:plugins [[cider/cider-nrepl "0.15.1"]
-                  [lein-bikeshed "0.2.0"]
+                  [lein-bikeshed "0.5.1"]
                   [lein-kibit "0.0.8"]
                   [lein-try "0.4.3"]
                   [codox "0.6.4"]
                   [venantius/yagni "0.1.4"]
-                  [jonase/eastwood "0.2.5"]]
+                  [jonase/eastwood "0.3.5"]]
           :aliases {"lint" ["do" "eastwood," "bikeshed"]}}
  :auth {:repository-auth {#"https://clojars.org/repo"
                          {:username "karlis"
