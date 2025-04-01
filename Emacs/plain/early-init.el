f4f46b8220dc670002ce9ad9e4a4c020a3e728b2
Karlis Lauva <k@karlis.me>
2025-04-01 23:37:56 +0300

    plain emacs: update early-init

--- Emacs/plain/early-init.el
+++ Emacs/plain/early-init.el
@@ -1,6 +1,10 @@
 (setq tool-bar-mode nil menu-bar-mode nil scroll-bar-mode nil) ; Hide the outdated icons
 (setq inhibit-splash-screen t) ; Remove the "Welcome to GNU Emacs" splash screen
 
+(setq gc-cons-threshold 10000000)
+
+;; Frame configuraton
+(setq frame-resize-pixelwise t)
 (setq default-frame-alist '((tool-bar-lines . 0)
                             (menu-bar-lines . 0)
                             (horizontal-scroll-bars . nil)
